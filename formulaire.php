5f7255eab866254aadedfda8276eecb2fdc59317
jfrancoisg <jf.gourdain@gmail.com>
2016-09-08 10:40:27 +0200

    Ajout d'un champ 'civilite' dans le formulaire

--- formulaire.php
+++ formulaire.php
@@ -2,10 +2,11 @@
 
 include('bdd.php');
 
-pg_query($bdd, 'INSERT INTO contact (nom, prenom, commentaire) VALUES 
+pg_query($bdd, 'INSERT INTO contact (nom, prenom, commentaire, civilite) VALUES 
                                 (\''.$_POST['nom'].'\',
                                  \''.$_POST['prenom'].'\', 
-                                 \''.$_POST['commentaire'].'\'
+                                 \''.$_POST['commentaire'].'\',
+                                 \''.$_POST['civilite'].'\'
                                  )');
                                  
 header('location: index.html');
